9bff7d926727cf1277173a9b17f8327a09572038
attila kekesi <kekesi.att@gmail.com>
2020-07-02 22:34:50 +0200

    Rreturns for Plots

--- FT_GUI_Fourier_Trafo.m
+++ FT_GUI_Fourier_Trafo.m
@@ -1,13 +1,14 @@
 %% Fourier GUI
 %  Fourier Trafo
 
-function [a,b,S,C,CS] = FT_GUI_Fourier_Trafo(funk,nft,x)
+function [a,b,S,C,CS,F] = FT_GUI_Fourier_Trafo(funk,nft,x)
 T = x(end)-x(1);
 a = zeros(1,nft);
 b = a;
-S = zeros(length(x));
+S = zeros(2,length(x),nft);
 C = S;
-CS = S;
+F = zeros(2,length(x),nft+1);               % 1. Zeile = 0 fuer Plot
+CS = zeros(1,length(x));
 s = @(x,k) sin((k-1)*x);
 c = @(x,k) cos((k-1)*x);
 for k = 1:1:nft
@@ -26,7 +27,11 @@ for k = 1:1:nft
     else
         a_0 = 1;
     end
-    C(k,:) = a_0*a(1,k)*c(x,k);
-    S(k,:) = b(1,k)*s(x,k);
-    CS(k,:) = C(k,:)+S(k,:);
+    C(1,:,k) = a_0*a(1,k)*c(x,k);               % y-Kompontente (Groesse)
+    C(2,:,k) = a_0*a(1,k)*-s(x,k);              % x-Kompontente (Groesse)
+    S(1,:,k) = b(1,k)*s(x,k);                   % y-Kompontente (Groesse)
+    S(2,:,k) = b(1,k)*c(x,k);                   % x-Kompontente (Groesse)
+    CS(k,:) = C(1,:,k)+S(1,:,k);                % y-Werte/nft
+    F(1,:,k+1) = F(1,:,k)+C(1,:,k)+S(1,:,k);    % y-Werte/nft summiert
+    F(2,:,k+1) = F(2,:,k)+C(2,:,k)+S(2,:,k);    % x-Werte/nft summiert
 end
