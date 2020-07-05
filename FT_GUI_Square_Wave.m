c1b9e050cf73da8b9f8de347fbf94a6be2b237d3
attila kekesi <kekesi.att@gmail.com>
2020-07-05 03:32:53 +0200

    shift of function

--- FT_GUI_Square_Wave.m
+++ FT_GUI_Square_Wave.m
@@ -1,7 +1,7 @@
 %% Fourier GUI
 %  Squar-Wave
 
-function [y] = FT_GUI_Square_Wave(x,w,N)
+function [y] = FT_GUI_Square_Wave(x,w,N,d)
 y = size(x);
 for n = 1:1:N
     for m = 1+fix((n-1)/N*length(x)):1:fix((n)/N*length(x))
@@ -11,5 +11,10 @@ for n = 1:1:N
             y(m) = -w;
         end
     end
+end
+tmp = [y y];
+d = fix(length(x)/d);
+for m = 1:1:length(x)
+    y(m) = tmp(m+d);
 end
 end
